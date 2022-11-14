92ecadc4f41f13a5f66b7ce3ce6e15d461a7b7a5
Robert <109295306+rarellano5280@users.noreply.github.com>
2022-11-13 21:37:58 -0700

    finished the index.js model

--- models/index.js
+++ models/index.js
@@ -5,12 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+// Defines the association between category and product
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+// Defines the relationship between categories and products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
+// Defines the association between product and tags as product_tag
+Product.belongsToMany(Tag, {
+  through: 'product_tag',
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+// Defines the association between tags and products as product_tag
+Tag.belongsToMany(Product, {
+  through: 'product_tag',
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
